6aa62381a1e149227c0393e18131a560871d9174
mathis-online <145270511+mathis-online@users.noreply.github.com>
2023-09-22 14:01:03 +0200

    Update views.py - commented out keyvault

--- openaiapp/views.py
+++ openaiapp/views.py
@@ -8,21 +8,21 @@ import openai
 from django.http import JsonResponse
 
 ### Start keyvault
-from azure.identity import DefaultAzureCredential
-from azure.keyvault.secrets import SecretClient
+#from azure.identity import DefaultAzureCredential
+#from azure.keyvault.secrets import SecretClient
 
-vault_url = "https://ma-app-test-key.vault.azure.net/"
-credential = DefaultAzureCredential()
+#vault_url = "https://ma-app-test-key.vault.azure.net/"
+#credential = DefaultAzureCredential()
 
-client = SecretClient(vault_url=vault_url, credential=credential)
+#client = SecretClient(vault_url=vault_url, credential=credential)
 
 #openai_api_key = client.get_secret("OPENAI-API-KEY").value
 #### End keyvault
 
 def get_openai_response(request):
     user_input = request.GET.get('prompt', '')
-    #openai.api_key = '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'  # Later, move this to a secure location like environment variables
-    openai_api_key = client.get_secret("OPENAI-API-KEY").value
+    openai.api_key = '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'  # Later, move this to a secure location like environment variables
+    #openai_api_key = client.get_secret("OPENAI-API-KEY").value
 
     response = openai.Completion.create(
       model="text-davinci-002",
